16d42ad45780619bf882b7b43ab8ddadc15978d8
Md Aminul Islam <mr.aminul118@gmail.com>
2024-10-22 08:41:04 +0600

    Environment Create

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Banner from "./Components/Banner/Banner";
+import Header from "./Components/Header/Header";
+
+
+export default function App() {
+  return (
+    <div className="container mx-auto">
+      {/* Header */}
+      <Header></Header>
+      {/* Banner */}
+      <Banner></Banner>
+      {/* Recepies */}
+    </div>
+  )
+}
