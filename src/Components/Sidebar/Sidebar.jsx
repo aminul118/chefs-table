114fda8cb753a53f7f2d81c056f50331075b1b92
Md Aminul Islam <mr.aminul118@gmail.com>
2024-10-22 15:51:05 +0600

    Want to cook functionalities added

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,7 +1,41 @@
 export default function Sidebar({ recipeQueue }) {
   return (
-    <div className="w-1/3 ml-4">
-      <h1>Want to cook: {recipeQueue.length}</h1>
+    <div className="w-1/3 ml-4 border text-center  ">
+      <h1 className="text-xl font-semibold">
+        Want to cook: {recipeQueue.length}
+      </h1>
+      {/* Want to cook table */}
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Time</th>
+              <th>Calories</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {recipeQueue.map((recipe, index) => (
+              <tr key={index} className="hover">
+                <th>{index + 1}</th>
+                <td>{recipe.recipe_name}</td>
+                <td>{recipe.preparing_time}</td>
+                <td>{recipe.calories}</td>
+                <td>
+                  <button onClick={()=>console.log('preparing')} className="btn bg-blue-600 text-white rounded-full">
+                    Preparing
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {/* Curently cook table */}
     </div>
   );
 }
